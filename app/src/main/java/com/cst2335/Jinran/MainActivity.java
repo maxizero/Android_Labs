2c604919bf974a3031b936d907da612f5d57068d
yujinran <yujinranup@gmail.com>
2022-02-11 19:20:48 -0500

    Step 2

--- app/src/main/java/com/cst2335/Jinran/MainActivity.java
+++ app/src/main/java/com/cst2335/Jinran/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         Button clickBtton = findViewById(R.id.ClickButton);
         clickBtton.setOnClickListener(view -> Toast.makeText( MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
