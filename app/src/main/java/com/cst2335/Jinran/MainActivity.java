60092fe2464f1261633168da39ce091528f9cffd
yujinran <yujinranup@gmail.com>
2022-02-11 19:16:46 -0500

    Step 1

--- app/src/main/java/com/cst2335/Jinran/MainActivity.java
+++ app/src/main/java/com/cst2335/Jinran/MainActivity.java
@@ -1,14 +1,42 @@
 package com.cst2335.Jinran;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.Switch;
+import android.view.View;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_main_linear);
+
+        Button clickBtton = findViewById(R.id.ClickButton);
+        clickBtton.setOnClickListener(view -> Toast.makeText( MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+
+
+
+        String switchOn = "Turn on!";
+        String switchOff = "Turn off!";
+        String undoStr = "Undo";
+        Switch simpleSwitch = (Switch)findViewById(R.id.simpleSwitch);
+        simpleSwitch.setOnCheckedChangeListener((CompoundButton bt, boolean isChecked) ->
+                Snackbar.make(simpleSwitch, (isChecked?switchOn:switchOff), Snackbar.LENGTH_LONG).
+                    setAction(undoStr, (click) -> simpleSwitch.setChecked(!isChecked)).show());
+
+
+
+
+
+            }
+                                                }
